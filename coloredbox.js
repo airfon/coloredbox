27fa68011124673022e2ee81afd8ef353b06bd2d
airfon <116084172+airfon@users.noreply.github.com>
2023-01-12 20:58:16 +0300

    Add files via upload

--- coloredbox.js
+++ coloredbox.js
@@ -1,6 +1,6 @@
 (function() {
- let template = document.createElement("template");
- template.innerHTML = `
+    let template = document.createElement("template");
+    template.innerHTML = `
  <style>
  :host {
  border-radius: 25px;
@@ -11,28 +11,33 @@
  } 
  </style>
  `;
- class ColoredBox extends HTMLElement {
- constructor() {
- super();
- let shadowRoot = this.attachShadow({mode: "open"});
- shadowRoot.appendChild(template.content.cloneNode(true));
- this.addEventListener("click", event => {
- var event = new Event("onClick");
- this.dispatchEvent(event);
- });
- this._props = {};
- }
- onCustomWidgetBeforeUpdate(changedProperties) {
- this._props = { ...this._props, ...changedProperties };
- }
- onCustomWidgetAfterUpdate(changedProperties) {
- if ("color" in changedProperties) {
- this.style["background-color"] = changedProperties["color"];
- }
- if ("opacity" in changedProperties) {
- this.style["opacity"] = changedProperties["opacity"];
- }
- }
- }
- customElements.define("com-sap-sample-coloredbox", ColoredBox);
+    class ColoredBox extends HTMLElement {
+        constructor() {
+            super();
+            let shadowRoot = this.attachShadow({
+                mode: "open"
+            });
+            shadowRoot.appendChild(template.content.cloneNode(true));
+            this.addEventListener("click", event => {
+                var event = new Event("onClick");
+                this.dispatchEvent(event);
+            });
+            this._props = {};
+        }
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            this._props = {
+                ...this._props,
+                ...changedProperties
+            };
+        }
+        onCustomWidgetAfterUpdate(changedProperties) {
+            if ("color" in changedProperties) {
+                this.style["background-color"] = changedProperties["color"];
+            }
+            if ("opacity" in changedProperties) {
+                this.style["opacity"] = changedProperties["opacity"];
+            }
+        }
+    }
+    customElements.define("com-sap-sample-coloredbox", ColoredBox);
 })();
--- coloredbox_builder.js
+++ coloredbox_builder.js
@@ -1,6 +1,6 @@
 (function() {
- let template = document.createElement("template");
- template.innerHTML = `
+    let template = document.createElement("template");
+    template.innerHTML = `
  <form id="form">
  <fieldset>
  <legend>Colored Box Properties</legend>
@@ -21,33 +21,35 @@ maxlength="5"></td>
  }
  </style>
  `;
- class ColoredBoxBuilderPanel extends HTMLElement {
- constructor() {
- super();
- this._shadowRoot = this.attachShadow({mode: "open"});
- this._shadowRoot.appendChild(template.content.cloneNode(true));
+    class ColoredBoxBuilderPanel extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({
+                mode: "open"
+            });
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-this._shadowRoot.getElementById("form").addEventListener("submit",
-this._submit.bind(this));
- }
- _submit(e) {
- e.preventDefault();
- this.dispatchEvent(new CustomEvent("propertiesChanged", {
- detail: {
- properties: {
- opacity: this.opacity
- }
- }
- }));
- }
- set opacity(newOpacity) {
- this._shadowRoot.getElementById("builder_opacity").value =
-newOpacity;
- }
- get opacity() {
- return this._shadowRoot.getElementById("builder_opacity").value;
- }
- }
- customElements.define("com-sap-sample-coloredbox-builder",
-ColoredBoxBuilderPanel);
+            this._shadowRoot.getElementById("form").addEventListener("submit",
+                this._submit.bind(this));
+        }
+        _submit(e) {
+            e.preventDefault();
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: {
+                        opacity: this.opacity
+                    }
+                }
+            }));
+        }
+        set opacity(newOpacity) {
+            this._shadowRoot.getElementById("builder_opacity").value =
+                newOpacity;
+        }
+        get opacity() {
+            return this._shadowRoot.getElementById("builder_opacity").value;
+        }
+    }
+    customElements.define("com-sap-sample-coloredbox-builder",
+        ColoredBoxBuilderPanel);
 })();
